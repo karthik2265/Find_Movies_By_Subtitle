fe48607212c59b5f370964e6af34dc652c1d1b16
karthik surya <karthiksurya2022@gmail.com>
2024-07-28 13:53:42 +0530

    finish

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import aiohttp
 import asyncio
 import os
 from dotenv import load_dotenv
+from opensubtitlescom import OpenSubtitles
 
 load_dotenv()
 
 # api keys
 THE_MOVIE_DB_API_KEY = os.getenv('THE_MOVIE_DB_API_KEY')
+OPEN_SUBTITLES_API_KEY = os.getenv('OPEN_SUBTITLES_API_KEY')
+OPEN_SUBTITLES_USERNAME = os.getenv('OPEN_SUBTITLES_USERNAME')
+OPEN_SUBTITLES_PASSWORD = os.getenv('OPEN_SUBTITLES_PASSWORD')
+
+# Initialize the OpenSubtitles client
+subtitles = OpenSubtitles("Find movie by subtitle", OPEN_SUBTITLES_API_KEY)
+
+# Log in
+subtitles.login(OPEN_SUBTITLES_USERNAME, OPEN_SUBTITLES_PASSWORD)
 
 
 # helper functions
@@ -22,6 +32,18 @@ async def fetch_imdb_id(session, url, params):
     async with session.get(url, params=params) as response:
         response = await response.json()
         return response['imdb_id']
+
+
+def fetch_subtitles_of_a_movie_and_check_if_subtitle_matched(movie, subtitle):
+    # we are only considering 1st page of the search result
+    response = subtitles.search(languages='en', imdb_id=movie['imdb_id'])
+    response = response.to_dict()
+    for res in response['data']:
+        movie_subtitles = subtitles.download_and_parse(file_id=res.file_id, sub_format='srt')
+        for movie_subtitle in movie_subtitles:
+            if subtitle in movie_subtitle.content.lower():
+                return True
+    return False
 
 
 async def search_for_movies_with_subtitle_from(start_year, end_year, subtitle):
@@ -45,21 +67,12 @@ async def search_for_movies_with_subtitle_from(start_year, end_year, subtitle):
                         'api_key': THE_MOVIE_DB_API_KEY,
                     }
                     movie['imdb_id'] = await fetch_imdb_id(session, get_imdb_id_url, get_imdb_id_params)
-                    print(movie['title'], movie['imdb_id'])
+                    if fetch_subtitles_of_a_movie_and_check_if_subtitle_matched(movie, subtitle):
+                        with open('result.txt', 'a') as file:
+                            file.write(f'title = {movie["title"]}, imdb_id = {movie["imdb_id"]} \n')
+                    break
                 break
             break
-
-
-
-async def check_if_movie_has_subtitle(title, sub_title):
-    # get subtitles from an external source
-
-    # format it to lowercase
-
-    # check if sub_title is found
-
-    # if found store the result in a text file
-    pass
 
 
 def main():
